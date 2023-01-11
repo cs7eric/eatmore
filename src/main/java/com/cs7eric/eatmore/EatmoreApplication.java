e9835ade359f5212b278f309a09f578ac2ba45a2
cs7eric <Cs7eric@outlook.com>
2023-01-11 19:00:40 +0800

    环境搭建

---
+++ src/main/java/com/cs7eric/eatmore/EatmoreApplication.java
@@ -0,0 +1,13 @@
+package com.cs7eric.eatmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatmoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatmoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cs7eric/eatmore/EatmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs7eric.eatmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatmoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
