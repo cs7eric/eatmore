3baf0b0f53d3242fd7fedc83812897751cbbd5c9
cs7eric <Cs7eric@outlook.com>
2023-03-30 21:50:36 +0800

    集成 redis

--- src/test/java/com/cs7eric/eatmore/EatmoreApplicationTests.java
+++ src/test/java/com/cs7eric/eatmore/EatmoreApplicationTests.java
@@ -1,13 +1,19 @@
 package com.cs7eric.eatmore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class EatmoreApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+        System.out.println(redisTemplate);
     }
 
 }
